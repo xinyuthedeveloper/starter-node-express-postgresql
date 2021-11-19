755e8f8658aebd0aaea62d5a2f6e52368f975f69
Xin Yu <wilsonyu0626@gmail.com>
2021-11-18 23:51:24 -0800

    worked on CRUD

--- src/suppliers/suppliers.router.js
+++ src/suppliers/suppliers.router.js
@@ -2,12 +2,16 @@ const router = require("express").Router({ mergeParams: true });
 const controller = require("./suppliers.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").post(controller.create).all(methodNotAllowed);
+router.route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
+
 
 router
-  .route("/:supplierId")
+  .route("/:supplierId([0-9]+)")
   .put(controller.update)
   .delete(controller.delete)
-  .all(methodNotAllowed);
+  .all(methodNotAllowed); 
 
 module.exports = router;
---
+++ src/suppliers/suppliers.service.js
@@ -0,0 +1,36 @@
+const knex = require("../db/connection");
+
+function create(supplier) {
+  return knex("suppliers")
+    .insert(supplier)
+    .returning("*")
+    .then((createdRecords) => createdRecords[0]);
+}
+
+function read(supplier_id) {
+  return knex("suppliers").select("*").where({ supplier_id }).first();
+}
+
+function update(updatedSupplier) {
+  return knex("suppliers")
+    .select("*")
+    .where({ supplier_id: updatedSupplier.supplier_id })
+    .update(updatedSupplier, "*")
+    .then((updatedRecords) => updatedRecords[0]);
+}
+
+function destroy(supplier_id) {
+  return knex("suppliers").where({ supplier_id }).del();
+} 
+
+function list() {
+  return knex("suppliers").select("*");
+}
+
+module.exports = {
+  list,
+  create,
+  read,
+  update,
+  delete: destroy, 
+};
