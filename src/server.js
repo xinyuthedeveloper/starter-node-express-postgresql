cfb830830ffcd80cf5cfef5e483464e6e8d3475b
Xin Yu <wilsonyu0626@gmail.com>
2021-11-21 18:43:58 -0800

    updated project

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
-const { PORT = 5000 } = process.env;
+const express = require('express')
 
 const app = require("./app");
+const router = express.Router();
+const PORT = process.env.PORT || 5000;
+
+
 const listener = () => console.log(`Listening on Port ${PORT}!`);
 app.listen(PORT, listener);
