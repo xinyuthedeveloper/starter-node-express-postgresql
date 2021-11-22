597bb816d690f3875bd517c93b788bff9f90fc46
Xin Yu <wilsonyu0626@gmail.com>
2021-11-21 19:08:11 -0800

    update

--- src/products/products.router.js
+++ src/products/products.router.js
@@ -1,25 +1,26 @@
 const router = require("express").Router({ mergeParams: true });
 const controller = require("./products.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+const cors = require("cors");
 
 router.route("/")
-    .get(controller.list)
+    .get(cors(), controller.list)
     .all(methodNotAllowed);
 
 router.route("/:productId([0-9]+)")
-    .get(controller.read)
+    .get(cors(), controller.read)
     .all(methodNotAllowed);
 
 router.route("/out-of-stock-count")
-    .get(controller.listOutOfStockCount)
+    .get(cors(), controller.listOutOfStockCount)
     .all(methodNotAllowed);
 
 router.route("/price-summary")
-    .get(controller.listPriceSummary)
+    .get(cors(), controller.listPriceSummary)
     .all(methodNotAllowed);
 
 router.route("/total-weight-by-product")
-    .get(controller.listTotalWeightByProduct)
+    .get(cors(), controller.listTotalWeightByProduct)
     .all(methodNotAllowed);
 
 module.exports = router;
